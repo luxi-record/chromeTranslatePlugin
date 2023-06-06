88c3ded407e390435d44de4a6539ef2c7377dcc4
1763486084 <1763486084@qq.com>
2023-06-06 17:18:10 +0800

    feature: 增加复制功能

--- content/App.jsx
+++ content/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     const [position, setPosition] = useState({ x: 0, y: 0 })
     const [show, setShow] = useState(false)
     const [content, setContent] = useState()
+    const [copyText, setCopyText] = useState('复制')
     const contentStyle = useMemo(() => {
         const style = content ? {
             padding: 5,
@@ -47,11 +48,7 @@ export default function App() {
     const selection = () => {
         const string = document.getSelection().toString()
         translateString.current = string
-        if (string) {
-            setShow(true)
-        } else {
-            setShow(false)
-        }
+        string && setShow(true)
     }
     // 阻止事件继续传递
     const prevent = (e) => {
@@ -62,6 +59,27 @@ export default function App() {
         setShow(false)
         setPosition({ x: e.pageX, y: e.pageY })
         setContent('')
+    }
+    const copy = async () => {
+        const copySuccess = await new Promise((reslove, reject) => {
+            let success = false
+            const input = document.createElement('input')
+            input.value = content
+            document.body.appendChild(input)
+            input.select() // 选择复制内容
+            if (document.execCommand) {
+                success = document.execCommand('copy') // 执行复制命令
+            } else {
+                navigator.clipboard.writeText(input.value)
+                success = true
+            }
+            document.body.removeChild(input)
+            reslove(success)
+        }).catch(() => false)
+        setCopyText(copySuccess ? '复制成功' : '复制失败')
+        setTimeout(() => {
+            setCopyText('复制')
+        }, 1500);
     }
     useEffect(() => {
         document.addEventListener('selectionchange', debounce(selection))
@@ -79,7 +97,8 @@ export default function App() {
                 maxWidth: document.documentElement.offsetWidth,
                 ...contentStyle
             }}>
-            {content ? content : <button onClick={translate}>翻译</button>}
+            {content ? <span>{content}</span> : <button onClick={translate}>翻译</button>}
+            {content && <button className={s.copy} onClick={copy}>{copyText}</button>}
         </div>
     )
 }
